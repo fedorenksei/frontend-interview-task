7599f041e86d790164c6d1bb2f68d2b1f9fab790
Aleksei Fedorenko <fedorenksei@gmail.com>
2024-03-27 18:42:30 +0400

    feat: add styles

--- src/components/post-card/index.mjs
+++ src/components/post-card/index.mjs
@@ -1,3 +1,4 @@
+import "./style.css";
 import { BaseComponent } from "../../utils/base-component.mjs";
 
 export class PostCard extends BaseComponent {
@@ -5,6 +6,10 @@ export class PostCard extends BaseComponent {
    * @param {{userId: string; id: string; title: string; body: string}} data
    */
   constructor({ userId, title, body }) {
-    super({ tag: "div", children: [userId, title, body] });
+    super({
+      tag: "div",
+      className: "post-card",
+      children: [userId, title, body],
+    });
   }
 }
--- src/components/posts-wrapper/index.mjs
+++ src/components/posts-wrapper/index.mjs
@@ -1,7 +1,12 @@
+import "./style.css";
 import { BaseComponent } from "../../utils/base-component.mjs";
 
 export class PostsWrapper extends BaseComponent {
   constructor(children) {
-    super({ tag: "div", children });
+    super({
+      tag: "div",
+      className: "cards-wrapper",
+      children,
+    });
   }
 }
